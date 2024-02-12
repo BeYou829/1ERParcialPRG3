f28040fbd818f272b57486cddaff5477e7d72a9b
BeYou829 <engelcontreras07@gmail.com>
2024-02-12 10:44:33 -0400

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _1ERParcialPRG3.Models;
+using _1ERParcialPRG3.Models.TableViewModel;
+namespace _1ERParcialPRG3.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Departament
+        public ActionResult Query()
+        {
+            List<mDeptTableViewModel> listDept = null;
+            using (dbmvcp3Entities db = new dbmvcp3Entities())
+            {
+                listDept = (from d in db.mDepartamentoes
+                            where d.Activo == 1
+                            orderby d.DeptoNombre
+                            
+                            select new mDeptTableViewModel
+                            {
+                                _Id = d.IdDepto,
+                                _DeptName = d.DeptoNombre,
+                                _ActualStatus = d.Activo
+                            }).ToList();
+            };
+                return View(listDept);
+        }
+    }
+}
---
+++ Models/TableViewModel/mDeptTableViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1ERParcialPRG3.Models.TableViewModel
+{
+	public class mDeptTableViewModel
+	{
+        public int _Id { get; set; }
+        public string _DeptName { get; set; }
+        public int _ActualStatus { get; set; }
+
+    }
+}
